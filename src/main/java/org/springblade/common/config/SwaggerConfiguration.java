cb7296b4ccf1c87effdb949a5c97718e7c4b4fa1
Eugene-Forest <1244303915@qq.com>
2021-11-19 10:24:35 +0800

    测试 swagger 文档的特殊端口放行以及端口调试功能

--- src/main/java/org/springblade/common/config/SwaggerConfiguration.java
+++ src/main/java/org/springblade/common/config/SwaggerConfiguration.java
@@ -9,6 +9,7 @@ import org.springblade.core.swagger.SwaggerProperties;
 import org.springblade.core.swagger.SwaggerUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -42,38 +43,59 @@ public class SwaggerConfiguration {
 	private final OpenApiExtensionResolver openApiExtensionResolver;
 
 	/**
-	 * 请求头部信息
+	 * 定义请求需要的请求头
 	 */
 	private final ApiKey clientInfo=new ApiKey("ClientInfo", "Authorization", "header");
 	private final ApiKey bladeAuth= new ApiKey("BladeAuth", "Blade-Auth", "header");
+	private final ApiKey bladeTenant=new ApiKey("TenantId", "Tenant-Id", "header");
+
 
 
 	@Bean
+	@Profile({"dev","test"})
 	public Docket testDocket() {
 		return docket("测试模块", Collections.singletonList(AppConstant.BASE_PACKAGES + ".test"));
 	}
 
+	@Bean
+	@Profile({"test"})
+	public Docket testDocket2() {
+		return docket("测试模块2", Collections.singletonList(AppConstant.BASE_PACKAGES + ".test"));
+	}
+
+	@Bean
+	@Profile({"!prod"})
+	public Docket testDocket3() {
+		return docket("测试模块3_非生产环境", Collections.singletonList(AppConstant.BASE_PACKAGES + ".test"));
+	}
 
 	/**
+	 * 模块的基本配置
+	 *
 	 * @param groupName 模块名
 	 * @param basePackages 模块包的根目录
 	 */
 	private Docket docket(String groupName, List<String> basePackages) {
+		// 接口文档的类型默认为 swagger version 2
 		return new Docket(DocumentationType.SWAGGER_2)
+			// 模块名定义
 			.groupName(groupName)
+			// 接口文档基本信息配置
 			.apiInfo(apiInfo())
+			// 扫描模块包的根目录下的接口
 			.select()
 			.apis(SwaggerUtil.basePackages(basePackages))
 			.paths(PathSelectors.any())
+			//构建
 			.build()
-			.securitySchemes(Lists.<SecurityScheme>newArrayList(
-				clientInfo,bladeAuth))
+			//添加请求需要的头部信息
+			.securitySchemes(Lists.<SecurityScheme>newArrayList(clientInfo,bladeAuth,bladeTenant))
+			// 使用Knife4j扩展
 			.extensions(openApiExtensionResolver.buildExtensions(groupName));
 	}
 
 	/**
-	 * 获取文档主页的基本信息
-	 * @return
+	 * swagger 文档主页的基本信息的配置，相关配置可以在项目运行配置文件中修改
 	 */
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
